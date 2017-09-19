b867e8fec921f68cd42dcddaa6f002b136cf9579
yarmendezCSUF <yarmendez@csu.fullerton.edu>
2017-09-18 18:35:45 -0700

    BubbleSort function is implemented.

--- bubble_sort/cls_bubbleSort_base.cs
+++ bubble_sort/cls_bubbleSort_base.cs
@@ -16,9 +16,22 @@ namespace bubble_sort
         [STAThread]
         static void Main()
         {
+
+            // 20170918 : YRM : Bubble Sort testing
+            //-------------------------------------
+            int[] sort_array_me = { 8, 1, 5, 7, 6, 7, 2, 9 };
+
+            int[] sorted_array = cls_bubbleSort_logic.BubbleSort(sort_array_me);
+
+            Console.Write(sorted_array);
+            //-------------------------------------
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new ui_bubbleSort());
+ 
+
         }
     }
 }
--- bubble_sort/cls_bubbleSort_logic.cs
+++ bubble_sort/cls_bubbleSort_logic.cs
@@ -11,5 +11,40 @@ namespace bubble_sort
 {
     class cls_bubbleSort_logic
     {
+
+        // 20170918 : YRM : Bubble sort function
+        // - Requires an int array as input
+        // - Returns bubble sorted int[] output
+
+        //// input array to sort
+        //int[] sort_array_me = { 8, 1, 5, 7, 6, 7, 2, 9 };
+
+        public static int[] BubbleSort(int[] sort_array)
+        {
+            int length = sort_array.Length;
+
+            int temp = sort_array[0];
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (sort_array[i] > sort_array[j])
+                    {
+                        temp = sort_array[i];
+
+                        sort_array[i] = sort_array[j];
+
+                        sort_array[j] = temp;
+                    }
+                }
+            }
+
+            // Return bubble sorted array
+            return sort_array;
+        }
+
+        
     }
+
 }
